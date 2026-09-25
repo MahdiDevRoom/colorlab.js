a8ade7108f040a8ac06ac8b6aa9884a8d41b36f0
MahdiDevRoom <mahdidevroom@gmail.com>
2026-09-25 18:16:36 +0330

    update colorlab.js

--- dist/colorlab.js
+++ dist/colorlab.js
@@ -35,76 +35,76 @@ export default class ColorLab {
         Public Methods
     -------------------------------------------- */
     // Tools
-    static detect() {}
-    static normalize() {}
-    static isValid() {}
-    static analyze() {}
-    static channels() {}
-    static random() {}
+    static detect() {};
+    static normalize() {};
+    static isValid() {};
+    static analyze() {};
+    static channels() {};
+    static random() {};
     
     // Color Conversions
-    static toKeyword() {}
-    static toRgb() {}
-    static toHex() {}
-    static toHsl() {}
-    static toHsv() {}
-    static toCmyk() {}
-    static toXyz() {}
-    static toLab() {}
-    static toLch() {}
-    static toOkLab() {}
-    static toOkLch() {}
-    static toOkHsl() {}
-    static toOkHsv() {}
+    static toKeyword() {};
+    static toRgb() {};
+    static toHex() {};
+    static toHsl() {};
+    static toHsv() {};
+    static toCmyk() {};
+    static toXyz() {};
+    static toLab() {};
+    static toLch() {};
+    static toOkLab() {};
+    static toOkLch() {};
+    static toOkHsl() {};
+    static toOkHsv() {};
     
     // Shading
-    static hue() {}
-    static okhue() {}
-    static tint() {}
-    static tone() {}
-    static shade() {}
-    static ray() {}
+    static hue() {};
+    static okhue() {};
+    static tint() {};
+    static tone() {};
+    static shade() {};
+    static ray() {};
     
     // Get Detail
-    static getRed() {}
-    static getGreen() {}
-    static getBlue() {}
-    static getAlpha() {}
-    static getHue() {}
-    static getSaturation() {}
-    static getLightness() {}
-    static getCyan() {}
-    static getMagenta() {}
-    static getYellow() {}
-    static getKey() {}
-    static getValue() {}
-    static getRay() {}
+    static getRed() {};
+    static getGreen() {};
+    static getBlue() {};
+    static getAlpha() {};
+    static getHue() {};
+    static getSaturation() {};
+    static getLightness() {};
+    static getCyan() {};
+    static getMagenta() {};
+    static getYellow() {};
+    static getKey() {};
+    static getValue() {};
+    static getRay() {};
     
     // Change Detail
-    static changeRed() {}
-    static changeGreen() {}
-    static changeBlue() {}
-    static changeAlpha() {}
-    static changeHue() {}
-    static changeSaturation() {}
-    static changeLightness() {}
-    static changeCyan() {}
-    static changeMagenta() {}
-    static changeYellow() {}
-    static changeKey() {}
-    static changeValue() {}
-    static changeRay() {}
+    static changeRed() {};
+    static changeGreen() {};
+    static changeBlue() {};
+    static changeAlpha() {};
+    static changeHue() {};
+    static changeSaturation() {};
+    static changeLightness() {};
+    static changeCyan() {};
+    static changeMagenta() {};
+    static changeYellow() {};
+    static changeKey() {};
+    static changeValue() {};
+    static changeRay() {};
     
     // Contrast
-    static getContrast() {}
-    static getContrastLevel() {}
-    static getBestContrastColor() {}
+    static getContrast() {};
+    static getContrastLevel() {};
+    static getBestContrastColor() {};
     
     // Harmonies
-    static getHarmonies() {}
+    static getHarmonies() {};
     
     // Mix
-    static mix() {}
+    static mix() {};
     
     // convert 
     static convert() {};
